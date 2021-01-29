ddce660fe90f3607d7e19de5f73ba665373584d3
Compotation <46853874+Compotation@users.noreply.github.com>
2021-01-28 20:05:07 -0500

    Spring Initializer with devtools and spring web

---
+++ src/main/kotlin/com/teamplusplus/bankplusplus/BankplusplusApplication.kt
@@ -0,0 +1,11 @@
+package com.teamplusplus.bankplusplus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankplusplusApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankplusplusApplication>(*args)
+}
