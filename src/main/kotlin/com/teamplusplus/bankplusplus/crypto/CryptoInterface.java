d7c771cb4332ee8566c31292e88deffecd39a5bf
GodBlessBigs <guijunzhang1@gmail.com>
2021-02-12 15:37:53 -0500

    Crypto implementation

---
+++ src/main/kotlin/com/teamplusplus/bankplusplus/crypto/CryptoInterface.java
@@ -0,0 +1,8 @@
+
+// Insert package above
+
+public interface CryptoInterface {
+    byte[] encrypt(byte[] data);
+
+    byte[] decrypt(byte[] data);
+}
---
+++ src/main/kotlin/com/teamplusplus/bankplusplus/crypto/CyptoMain.java
@@ -0,0 +1,21 @@
+
+// Insert package above
+
+public class CyptoMain {
+
+    public static void main(String[] args) {
+        Crypto crypto = new Crypto();
+
+        String data = "guijun";
+
+        String enc = new String(crypto.encrypt(data.getBytes()));
+        String dec = new String(crypto.decrypt(enc.getBytes()));
+
+        System.out.println("Original  : " + data);
+        System.out.println("Encrypted : " + enc);
+        // dec should be the same as data
+        System.out.println("Decrypted : " + dec);
+
+    }
+
+}
