2bcfbadb6fe3a0d84920b3af994f3ffe950854e7
Compotation <46853874+Compotation@users.noreply.github.com>
2021-02-16 20:55:38 -0500

    display dropdown of stocks

---
+++ src/components/BuyStock.vue
@@ -0,0 +1,24 @@
+<template>
+  <label for="stock-select"></label>
+
+  <select id="stock-select" name="stocks">
+    <option value="">Please select a stock</option>
+
+    <option v-for="stock in stocks" :key="stock" value="{{stock}}">
+      {{ stock }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "BuyStock",
+  data() {
+    return {
+      stocks: ["GOOGL", "GME", "APPL"],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
