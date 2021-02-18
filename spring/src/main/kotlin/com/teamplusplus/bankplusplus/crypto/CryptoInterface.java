8d0d515ded76e2dc4d2a6a44833180d0f54bff20
GodBlessBigs <guijunzhang1@gmail.com>
2021-02-18 15:50:56 -0500

    cypher

--- spring/src/main/kotlin/com/teamplusplus/bankplusplus/crypto/CryptoInterface.java
+++ spring/src/main/kotlin/com/teamplusplus/bankplusplus/crypto/CryptoInterface.java
@@ -1,7 +1,7 @@
 package com.teamplusplus.bankplusplus.crypto;
 
 public interface CryptoInterface {
-    byte[] encrypt(byte[] data);
+    byte[] encrypt(byte[] data, int keyOne, int keyTwo, int keyThree);
 
-    byte[] decrypt(byte[] data);
+    byte[] decrypt(byte[] data, int keyOne, int keyTwo, int keyThree);
 }
