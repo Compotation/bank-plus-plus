7599b8a0b0491f60457212bd506a4ba7ba21cadd
Compotation <46853874+Compotation@users.noreply.github.com>
2021-02-17 20:08:22 -0500

    router with parameter for listing transactions

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home";
 import Accounts from "@/views/Accounts";
+import AccountView from "@/views/AccountView";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/accounts", component: Accounts },
+  { path: "/accounts/:id", component: AccountView },
 ];
 
 const router = createRouter({
---
+++ src/views/AccountView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Account {{ $route.params.id }}</h1>
+  <Transactions />
+</template>
+
+<script>
+import Transactions from "@/components/Transactions";
+
+export default {
+  name: "AccountView",
+  components: { Transactions },
+};
+</script>
+
+<style scoped></style>
