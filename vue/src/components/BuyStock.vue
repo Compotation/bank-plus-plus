886e642f129ef1139ae94b225527ae2577fc6cb3
Compotation <46853874+Compotation@users.noreply.github.com>
2021-02-21 18:17:44 -0500

    select account to buy stock for

--- vue/src/components/BuyStock.vue
+++ vue/src/components/BuyStock.vue
@@ -1,11 +1,19 @@
 <template>
   <label for="stock-select">Select a stock to buy:</label>
-
   <select id="stock-select" name="stocks">
     <option value="">Please select a stock</option>
 
     <option v-for="stock in stocks" :key="stock" value="{{stock}}">
       {{ stock }}
+    </option>
+  </select>
+
+  <label for="account-select">Account:</label>
+  <select id="account-select" name="accounts">
+    <option value="">Please select an account</option>
+
+    <option v-for="account in accounts" :key="account" :value="account">
+      {{ account }}
     </option>
   </select>
 
@@ -18,6 +26,7 @@ export default {
   data() {
     return {
       stocks: ["GOOGL", "GME", "APPL"],
+      accounts: { rand1: 41.2, rand2: 0.0 },
     };
   },
 };
