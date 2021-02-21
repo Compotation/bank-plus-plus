c8c65502b10112a7759a2f2784543ee6f7f2033d
Compotation <46853874+Compotation@users.noreply.github.com>
2021-02-21 18:04:05 -0500

    add header with navbar

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/accounts">Accounts</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped></style>
