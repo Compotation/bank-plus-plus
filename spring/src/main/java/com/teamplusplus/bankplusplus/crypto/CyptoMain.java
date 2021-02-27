3ac9a8daf70d15e37dc4711108c70804f73a751e
Compotation <46853874+Compotation@users.noreply.github.com>
2021-02-27 02:03:58 -0500

    inserted 1 as parameters for encrypt b/c they were missing

--- spring/src/main/java/com/teamplusplus/bankplusplus/crypto/CyptoMain.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/crypto/CyptoMain.java
@@ -7,8 +7,8 @@ public class CyptoMain {
 
         String data = "Quincy Holmer"; // modify data here
 
-        String enc = new String(crypto.encrypt(data.getBytes()));
-        String dec = new String(crypto.decrypt(enc.getBytes()));
+        String enc = new String(crypto.encrypt(data.getBytes(), 1, 1, 1));
+        String dec = new String(crypto.decrypt(enc.getBytes(), 1, 1, 1));
 
         System.out.println("Original  : " + data);
         System.out.println("Encrypted : " + enc);
