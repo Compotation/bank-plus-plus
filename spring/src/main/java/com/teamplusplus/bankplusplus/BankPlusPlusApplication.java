887824f96c2ff17a2622823dc7f96c3568f8a097
Compotation <46853874+Compotation@users.noreply.github.com>
2021-03-28 15:39:38 -0400

    set up cors for localhost:8000

--- spring/src/main/java/com/teamplusplus/bankplusplus/BankPlusPlusApplication.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/BankPlusPlusApplication.java
@@ -2,10 +2,23 @@ package com.teamplusplus.bankplusplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BankPlusPlusApplication {
   public static void main(String[] args) {
     SpringApplication.run(BankPlusPlusApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:8000");
+      }
+    };
   }
 }
