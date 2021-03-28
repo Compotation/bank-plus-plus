efae1bffd805dd0b9691ce0b6ac7f2a7dbeb983e
Compotation <46853874+Compotation@users.noreply.github.com>
2021-03-27 22:32:55 -0400

    encrypt and decrypt take in strings

--- spring/src/main/java/com/teamplusplus/bankplusplus/crypto/Crypto.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/crypto/Crypto.java
@@ -16,11 +16,16 @@ public class Crypto {
         return new String(enc);
     }
 
-    public String decrypt(byte[] data) {
-        byte[] enc = new byte[data.length];
+    public String encrypt(String data) {
+        return encrypt(data.getBytes());
+    }
 
-        for (int i = 0; i < data.length; i++) {
-            enc[i] = (byte) ((i % keys[0] == 0) ? data[i] - keys[1] : data[i] + keys[2]);
+    public String decrypt(String data) {
+        byte[] convertedData = data.getBytes();
+        byte[] enc = new byte[convertedData.length];
+
+        for (int i = 0; i < convertedData.length; i++) {
+            enc[i] = (byte) ((i % keys[0] == 0) ? convertedData[i] - keys[1] : convertedData[i] + keys[2]);
         }
         return new String(enc);
     }
