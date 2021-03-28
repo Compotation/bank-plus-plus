f477f5fa6ac8ed9cafc15195bcbe579312974353
Compotation <46853874+Compotation@users.noreply.github.com>
2021-03-28 14:24:39 -0400

    more encryption methods

--- spring/src/main/java/com/teamplusplus/bankplusplus/crypto/Crypto.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/crypto/Crypto.java
@@ -1,32 +1,38 @@
 package com.teamplusplus.bankplusplus.crypto;
 
 public class Crypto {
-    private final int[] keys;
+  private final int[] keys;
 
-    public Crypto(int[] keys) {
-        this.keys = keys;
+  public Crypto(int[] keys) {
+    this.keys = keys;
+  }
+
+  public String encrypt(byte[] data) {
+    byte[] enc = new byte[data.length];
+
+    for (int i = 0; i < data.length; i++) {
+      enc[i] = (byte) ((i % keys[0] == 0) ? data[i] + keys[1] : data[i] - keys[2]);
     }
+    return new String(enc);
+  }
 
-    public String encrypt(byte[] data) {
-        byte[] enc = new byte[data.length];
+  public String encrypt(String data) {
+    return encrypt(data.getBytes());
+  }
 
-        for (int i = 0; i < data.length; i++) {
-            enc[i] = (byte) ((i % keys[0] == 0) ? data[i] + keys[1] : data[i] - keys[2]);
-        }
-        return new String(enc);
+  public String encrypt(Double data) {
+    return encrypt(String.valueOf(data));
+  }
+
+//  public String
+
+  public String decrypt(String data) {
+    byte[] convertedData = data.getBytes();
+    byte[] enc = new byte[convertedData.length];
+
+    for (int i = 0; i < convertedData.length; i++) {
+      enc[i] = (byte) ((i % keys[0] == 0) ? convertedData[i] - keys[1] : convertedData[i] + keys[2]);
     }
-
-    public String encrypt(String data) {
-        return encrypt(data.getBytes());
-    }
-
-    public String decrypt(String data) {
-        byte[] convertedData = data.getBytes();
-        byte[] enc = new byte[convertedData.length];
-
-        for (int i = 0; i < convertedData.length; i++) {
-            enc[i] = (byte) ((i % keys[0] == 0) ? convertedData[i] - keys[1] : convertedData[i] + keys[2]);
-        }
-        return new String(enc);
-    }
+    return new String(enc);
+  }
 }
