bd47e19ffc8cb8eb268ef517c8b48b4a79701ae3
Compotation <46853874+Compotation@users.noreply.github.com>
2021-03-30 15:27:26 -0400

    export data from accounts

--- spring/src/main/java/com/teamplusplus/bankplusplus/controllers/AccountController.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/controllers/AccountController.java
@@ -1,5 +1,7 @@
 package com.teamplusplus.bankplusplus.controllers;
 
+import com.teamplusplus.bankplusplus.crypto.Crypto;
+import com.teamplusplus.bankplusplus.crypto.DataEncrypt;
 import com.teamplusplus.bankplusplus.models.Account;
 import com.teamplusplus.bankplusplus.models.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +9,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class AccountController {
   private final AccountRepository accountRepository;
+  private final DataEncrypt dataEncrypt;
 
   public AccountController(AccountRepository accountRepository) {
     this.accountRepository = accountRepository;
+    this.dataEncrypt = new DataEncrypt(new Crypto(new int[]{(int) (Math.random() * 40), (int) (Math.random() * 99), (int) (Math.random() * 50)}));
   }
 
   @GetMapping("/accounts")
@@ -25,5 +31,12 @@ public class AccountController {
   @PostMapping("/accounts")
   public void addAccount(@RequestParam String name) {
     accountRepository.addAccount(name);
+  }
+
+  @GetMapping("/export")
+  public List<Map<String, String>> encryptExport() {
+    List<Map<String, String>> export = new ArrayList<>();
+    accountRepository.getAccounts().forEach(account -> export.add(dataEncrypt.encryptAccount(account)));
+    return export;
   }
 }
