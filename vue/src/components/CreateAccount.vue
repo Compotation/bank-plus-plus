cc35c0be3a702c4ac53d9c8bc17ec915860a147c
Compotation <46853874+Compotation@users.noreply.github.com>
2021-03-30 14:49:21 -0400

    way to create account

---
+++ vue/src/components/CreateAccount.vue
@@ -0,0 +1,29 @@
+<template>
+  <label for="name">Name of Account</label>
+  <input id="name" v-model="accountName" required type="text" />
+
+  <button @click="createAccount">Create</button>
+</template>
+
+<script>
+export default {
+  name: "CreateAccount",
+  data() {
+    return {
+      accountName: "",
+    };
+  },
+  methods: {
+    createAccount() {
+      fetch(
+        process.env.VUE_APP_API + "/accounts" + "?name=" + this.accountName,
+        {
+          method: "POST",
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- vue/src/views/ViewAllAccounts.vue
+++ vue/src/views/ViewAllAccounts.vue
@@ -1,13 +1,15 @@
 <template>
   <AccountsList></AccountsList>
+  <CreateAccount></CreateAccount>
 </template>
 
 <script>
 import AccountsList from "@/components/AccountsList";
+import CreateAccount from "@/components/CreateAccount";
 
 export default {
   name: "Accounts",
-  components: { AccountsList },
+  components: { CreateAccount, AccountsList },
 };
 </script>
 
