d197a0d4e637d4a03b8f097a2a8a89cd5759f40b
Compotation <46853874+Compotation@users.noreply.github.com>
2021-04-01 19:21:16 -0400

    fixed error where stock table wouldn't load

--- vue/src/components/AvailableStocks.vue
+++ vue/src/components/AvailableStocks.vue
@@ -1,22 +1,24 @@
 <template>
-  <Button @click="getStocks">Refresh</Button>
+  <div>
+    <Button @click="getStocks">Refresh</Button>
 
-  <table>
-    <thead>
-      <tr>
-        <th>Company</th>
-        <th>Ticker</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="stock in stocks" :key="stock">
-        <td>{{ stock[0] }}</td>
-        <td>{{ stock[1] }}</td>
-        <td>{{ stock[2] }}</td>
-      </tr>
-    </tbody>
-  </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Company</th>
+          <th>Ticker</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="stock in stocks" :key="stock">
+          <td>{{ stock[0] }}</td>
+          <td>{{ stock[1] }}</td>
+          <td>{{ stock[2] }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -26,7 +28,7 @@ export default {
     return {
       data() {
         return {
-          stocks: [],
+          stocks: [[2, 3, 3]],
         };
       },
     };
