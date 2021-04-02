4b0d6b6e4ba1a3cd6e9a81cba7c33ef2e987db18
Compotation <46853874+Compotation@users.noreply.github.com>
2021-04-01 21:21:44 -0400

    consolidate components

--- vue/src/router.js
+++ vue/src/router.js
@@ -2,14 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home";
 import ViewAllAccounts from "@/views/ViewAllAccounts";
 import ManageAccount from "@/views/ManageAccount";
-import Buy from "@/views/Buy";
+import BuyStock from "@/views/BuyStock";
 import StockInfo from "@/views/StockInfo";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/accounts", component: ViewAllAccounts },
   { path: "/accounts/:id", component: ManageAccount },
-  { path: "/buy", component: Buy },
+  { path: "/buy", component: BuyStock },
   { path: "/stocks", component: StockInfo },
 ];
 
