30a547b2b20b8202f0ca0fd0fff04dcc7555c065
Compotation <46853874+Compotation@users.noreply.github.com>
2021-04-03 18:59:55 -0400

    use static stocks field

--- spring/src/main/java/com/teamplusplus/bankplusplus/controllers/TransactionController.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/controllers/TransactionController.java
@@ -2,49 +2,26 @@ package com.teamplusplus.bankplusplus.controllers;
 
 import com.teamplusplus.bankplusplus.models.AccountRepository;
 import com.teamplusplus.bankplusplus.models.ArrayMethods;
-import com.teamplusplus.bankplusplus.models.Stock;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TransactionController {
   private final AccountRepository accountRepository;
-  private final String[][] stocks;
 
   public TransactionController(AccountRepository accountRepository) {
     this.accountRepository = accountRepository;
-    stocks = new String[][]{
-                            // without Ticker symbol
-                            new Stock("Tesla", 200.50).toArray(),
-                            new Stock("Apple", 985.00).toArray(),
-                            new Stock("Microsoft", 240.35).toArray(),
-                            new Stock("Facebook", 300.00).toArray(),
-                            new Stock("Ubisoft", 15.00).toArray(),
-                            new Stock("Blizzard", 95.50).toArray(),
-                            new Stock("Google", 2005.00).toArray(),
-                            new Stock("EA", 140.00).toArray(),
-                            new Stock("IBM", 133.00).toArray(),
-                            new Stock("Pied Piper", 0.07).toArray(),
-                            new Stock("Aviato", 12.50).toArray(),
-                            // with Ticker symbol
-                            new Stock("AT&T", "T", 30.47).toArray(),
-                            new Stock("Home Depot Inc", "HD", 307.75).toArray(),
-                            new Stock("Johnson & Johnson", "JNJ", 160.00).toArray(),
-                            new Stock("Ford Motor Company", "F", 12.00).toArray(),
-                            new Stock("Costco Wholesale", "COST", 355.00).toArray(),
-                            new Stock("Citigroup", "C", 73.00).toArray(),
-                            new Stock("Dell Technologies", "DELL", 90.00).toArray(),
-                            };
+
   }
 
   @PostMapping("/{accountId}/transactions")
   public void buy(@PathVariable String accountId, @RequestParam String stock, @RequestParam int amount) {
     for (int i = 0; i < amount; i++) {
-      accountRepository.getAccount(accountId).buyStock(stock, ArrayMethods.findStockPrice(stocks, stock));
+      accountRepository.getAccount(accountId).buyStock(stock, ArrayMethods.findStockPrice(stock));
     }
   }
 
   @GetMapping("/stocks")
   public String[][] getStocks() {
-    return stocks;
+    return ArrayMethods.stocks;
   }
 }
--- spring/src/main/java/com/teamplusplus/bankplusplus/models/ArrayMethods.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/models/ArrayMethods.java
@@ -4,10 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayMethods {
+  public static final String[][] stocks = new String[][]{
+      // without Ticker symbol
+      new Stock("Tesla", 200.50).toArray(),
+      new Stock("Apple", 985.00).toArray(),
+      new Stock("Microsoft", 240.35).toArray(),
+      new Stock("Facebook", 300.00).toArray(),
+      new Stock("Ubisoft", 15.00).toArray(),
+      new Stock("Blizzard", 95.50).toArray(),
+      new Stock("Google", 2005.00).toArray(),
+      new Stock("EA", 140.00).toArray(),
+      new Stock("IBM", 133.00).toArray(),
+      new Stock("Pied Piper", 0.07).toArray(),
+      new Stock("Aviato", 12.50).toArray(),
+      // with Ticker symbol
+      new Stock("AT&T", "T", 30.47).toArray(),
+      new Stock("Home Depot Inc", "HD", 307.75).toArray(),
+      new Stock("Johnson & Johnson", "JNJ", 160.00).toArray(),
+      new Stock("Ford Motor Company", "F", 12.00).toArray(),
+      new Stock("Costco Wholesale", "COST", 355.00).toArray(),
+      new Stock("Citigroup", "C", 73.00).toArray(),
+      new Stock("Dell Technologies", "DELL", 90.00).toArray(),
+  };
+
   /**
    * Finds the price of a stock. Returns -1 if stock not found
    */
-  public static double findStockPrice(String[][] stocks, String stockName) {
+  public static double findStockPrice(String stockName) {
     for (String[] stock : stocks) {
       if (stock[1].equals(stockName)) {
         return Double.parseDouble(stock[2]);
