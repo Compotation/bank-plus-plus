db125e27cedb2d08568c1aab2c4791256bff97d8
GodBlessBigs <guijunzhang1@gmail.com>
2021-04-06 15:54:44 -0400

    Update ArrayMethods.java

--- spring/src/main/java/com/teamplusplus/bankplusplus/models/ArrayMethods.java
+++ spring/src/main/java/com/teamplusplus/bankplusplus/models/ArrayMethods.java
@@ -6,25 +6,25 @@ import java.util.List;
 public class ArrayMethods {
   public static final String[][] stocks = new String[][]{
       // without Ticker symbol
-      new Stock("Tesla", 200.50).toArray(),
-      new Stock("Apple", 985.00).toArray(),
-      new Stock("Microsoft", 240.35).toArray(),
-      new Stock("Facebook", 300.00).toArray(),
-      new Stock("Ubisoft", 15.00).toArray(),
-      new Stock("Blizzard", 95.50).toArray(),
-      new Stock("Google", 2005.00).toArray(),
-      new Stock("EA", 140.00).toArray(),
-      new Stock("IBM", 133.00).toArray(),
-      new Stock("Pied Piper", 0.07).toArray(),
-      new Stock("Aviato", 12.50).toArray(),
+      new Stock("Tesla", 690.00, 0.02).toArray(),
+      new Stock("Apple", 126.00, 0.01).toArray(),
+      new Stock("Microsoft", 240.35, 0.03).toArray(),
+      new Stock("Facebook", 300.00, 0.02).toArray(),
+      new Stock("Ubisoft", 15.00, 0.01).toArray(),
+      new Stock("Blizzard", 95.50, 0.02).toArray(),
+      new Stock("Google", 2005.00, 0.09).toArray(),
+      new Stock("EA", 140.00, 0.01).toArray(),
+      new Stock("IBM", 133.00, 0.01).toArray(),
+      new Stock("Pied Piper", 0.07, 2.6).toArray(),
+      new Stock("Aviato", 12.50, 0.01).toArray(),
       // with Ticker symbol
-      new Stock("AT&T", "T", 30.47).toArray(),
-      new Stock("Home Depot Inc", "HD", 307.75).toArray(),
-      new Stock("Johnson & Johnson", "JNJ", 160.00).toArray(),
-      new Stock("Ford Motor Company", "F", 12.00).toArray(),
-      new Stock("Costco Wholesale", "COST", 355.00).toArray(),
-      new Stock("Citigroup", "C", 73.00).toArray(),
-      new Stock("Dell Technologies", "DELL", 90.00).toArray(),
+      new Stock("AT&T", "T", 30.47, 0.01).toArray(),
+      new Stock("Home Depot Inc", "HD", 307.75, 0.01).toArray(),
+      new Stock("Johnson & Johnson", "JNJ", 160.00, 0.02).toArray(),
+      new Stock("Ford Motor Company", "F", 12.00, 0.01).toArray(),
+      new Stock("Costco Wholesale", "COST", 355.00, 0.01).toArray(),
+      new Stock("Citigroup", "C", 73.00, 0.01).toArray(),
+      new Stock("Dell Technologies", "DELL", 90.00, 0.01).toArray(),
   };
 
   /**
